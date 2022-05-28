a6cc44a7dd94ce02addcaf6a98592f2d7409684d
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-28 16:38:45 -0400

    Second commit

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-console.log('May Node be with you')
+console.log('May Node be with you');
+
+
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html');
-});
-
-app.post('/quotes', (req, res) => {
-  console.log('Hellooooooooooooooooo!')
-})
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 
-app.listen(3001, () => {
-  console.log('listening on http://localhost:3001/');
-});
+MongoClient.connect(process.env.MONGODB_URI).then(client => {
+	const db = client.db('star-wars-quotes');
+	const quotesCollection = db.collection('quotes');
+
+	app.get('/', (request, response) => {
+		db.collection('quotes').find().toArray()
+			.then(results => {
+				response.render('index.ejs', { quotes: results });
+			})
+			.catch(error => console.error(error))
+	});
+
+	app.post('/quotes', (request, response) => {
+		quotesCollection.insertOne(request.body)
+			.then(result => {
+				console.log(result);
+				response.redirect('/');
+			})
+			.catch(error => console.error(error));
+	});
+
+	app.listen(3001, () => {
+		console.log('Listening on http://localhost:3001/');
+	});
+}).catch(console.error);
