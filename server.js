ddcdf8cec42edaf50444908632af61ada784930a
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-28 19:04:24 -0400

    Properly handle async errors

--- server.js
+++ server.js
@@ -19,28 +19,28 @@ MongoClient.connect(process.env.MONGODB_URI).then(client => {
 	const db = client.db('star-wars-quotes');
 	const quotesCollection = db.collection('quotes');
 
-	app.get('/', (_, response) => {
+	app.get('/', (_, response, next) => {
 		return quotesCollection.find().toArray().then(results =>
 			response.render('index.ejs', { quotes: results })
-		)
+		).catch(next);
 	});
 
-	app.post('/quotes', (request, response) => {
+	app.post('/quotes', (request, response, next) => {
 		return quotesCollection.insertOne(request.body).then(() =>
 			response.redirect('/')
-		);
+		).catch(next);
 	})
 
-	app.delete('/quotes', (request, response) => {
+	app.delete('/quotes', (request, response, next) => {
 		return quotesCollection.deleteOne({ _id: new mongodb.ObjectId(request.body.quoteID) }).then(result =>
 			response.status(result.deletedCount === 0 ? 404 : 200).end()
-		);
+		).catch(next);
 	});
 
-	app.put('/quotes', (request, response) => {
+	app.put('/quotes', (request, response, next) => {
 		return quotesCollection.findOneAndUpdate({ _id: new mongodb.ObjectId(request.body.quoteID) }, { $set: { name: request.body.name, quote: request.body.quote } }, { upsert: true }).then(result =>
 			response.send(result.value)
-		);
+		).catch(next);
 	});
 
 	app.listen(3001, () => {
