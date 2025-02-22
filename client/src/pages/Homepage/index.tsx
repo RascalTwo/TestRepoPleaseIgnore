1929e6666b8cabb16019968edf4b1c2acb3b2327
Rascal Two <therealrascaltwo@gmail.com>
2025-02-22 11:50:58 -0500

    Use Vite PWA plugin

--- client/src/pages/Homepage/index.tsx
+++ client/src/pages/Homepage/index.tsx
@@ -10,7 +10,7 @@ type HomepageProps = {
 const Homepage = ({ setUser, isLoggedIn }: HomepageProps) => {
   return (
     <div>
-      2
+      3
       <main>
         {isLoggedIn ? (
           <div
