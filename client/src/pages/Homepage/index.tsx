8a1afa30444e8bb9207c38eb6d658574e89b169e
Rascal Two <therealrascaltwo@gmail.com>
2025-02-22 11:38:34 -0500

    Update to 2

--- client/src/pages/Homepage/index.tsx
+++ client/src/pages/Homepage/index.tsx
@@ -10,7 +10,7 @@ type HomepageProps = {
 const Homepage = ({ setUser, isLoggedIn }: HomepageProps) => {
   return (
     <div>
-      1
+      2
       <main>
         {isLoggedIn ? (
           <div
