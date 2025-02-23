0fab5baf0782e0fbc4ea1aa92783c81d05f796a4
Rascal Two <therealrascaltwo@gmail.com>
2025-02-23 00:47:44 -0500

    Bump to 17

--- client/src/pages/Homepage/index.tsx
+++ client/src/pages/Homepage/index.tsx
@@ -10,7 +10,7 @@ type HomepageProps = {
 const Homepage = ({ setUser, isLoggedIn }: HomepageProps) => {
   return (
     <div>
-      16
+      17
       <main>
         {isLoggedIn ? (
           <div
